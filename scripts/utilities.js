f2bc373637f1c6a9f12e3e3f4f5cc0eb9601d8f7
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-11-12 16:54:04 +0200

    [core]: created a function that returns a random color

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,3 +1,7 @@
 export function playAudio(audioPath) {
   new Audio(audioPath).play();
 }
+
+export function getRandomColor(pattern) {
+  return pattern[Math.floor(Math.random() * pattern.length)];
+}
